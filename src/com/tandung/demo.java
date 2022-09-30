74d487cc0acf052be1484ff280796c5a9b49a4c7
TanDung <nguyentandung9111@gmail.com>
2022-09-30 23:14:13 +0700

    update code for feature-test

--- src/com/tandung/demo.java
+++ src/com/tandung/demo.java
@@ -4,5 +4,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hello world11");
 		System.out.println("hello world 2");
+		int a = 5,b=3;
+		System.out.println(a+b);
 	}
 }
