b453cf1b3f553788aa6f8b7b3cf17ede3ce43185
TanDung <nguyentandung9111@gmail.com>
2022-09-30 22:33:14 +0700

    sua code demo

--- src/com/tandung/demo.java
+++ src/com/tandung/demo.java
@@ -2,6 +2,7 @@ package com.tandung;
 
 public class demo {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world11");
+		System.out.println("hello world 2");
 	}
 }
